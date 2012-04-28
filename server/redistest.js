095941d05ffda7f48bdc0a8628e7f7bbed30ba6d
Svenn-Arne Dragly <s@dragly.com>
2012-04-28 03:17:48 +0200

    Started work on redis implementation. Working for user creation / checking. Caused placeboats to crash.

--- server/redistest.js
+++ server/redistest.js
@@ -3,13 +3,29 @@ client = redis.createClient();
 client.on("error", function (err) {
     console.log("Error " + err);
 });
+var lars = "petter";
+var userID = 0;
+client.incr("global:nextUserID", function(err, userIDa) {
+    console.log(err);
+    console.log(userIDa);
+    userID = userIDa;
+    console.log(userID + " " + userIDa + " woow")
+    client.hmset("user:" + userID, "userID", userID, "username", "lars petter", "password", "1230", function(err, obj1) {
+         console.log(err);
+         console.log(obj1);
+         console.log("Looking for " + "user:" + userID)
+         client.hgetall("user:" + userID, function (err, obj) {
+             console.log(obj);
+             client.quit();
+         });
+    });
+});
 
-client.hmset("users:1000", "userID", "0", "username", "lars", "password", "1234");
-client.hmset("user:1001", "userID", "1", "username", "kalle", "password", "abcd");
-client.hgetall("user:1000", function (err, obj) {
-    console.log(obj);
-});
-client.hgetall("user:1001", function (err, obj) {
-    console.log(obj);
-});
-client.quit();
+//client.hmset("user:10010", "userID", "1", "username", "ABA", "password", "abcd");
+//client.hgetall("user:10010", function (err, obj) {
+//    console.log(obj.username);
+//});
+//client.hgetall("user:1001", function (err, obj) {
+//    console.log(obj);
+//});
+
--- server/usermanager.js
+++ server/usermanager.js
@@ -1,3 +1,6 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
 var User = require("./user").User;
 
 function UserManager() {
@@ -5,13 +8,14 @@ function UserManager() {
     this.currentUserID = 0;
 }
 
-UserManager.prototype.addUser = function() {
-    var user = new User();
-    user.userID = this.currentUserID;
-    user.password = "12345";
-    this.users[this.currentUserID] = user;
-    this.currentUserID++;
-    return user;
+UserManager.prototype.addUser = function(mycallback) {
+    client.incr("global:nextUserID", function(err, userID) {
+        client.hmset("user:" + userID, "userID", userID, "username", "newUser", "password", "12345", function(err, user) {
+            client.hgetall("user:" + userID, function(err, user) {
+                mycallback(user);
+            });
+        });
+    });
 }
 
 UserManager.prototype.auth = function (userID, key) {
@@ -24,20 +28,13 @@ UserManager.prototype.auth = function (userID, key) {
         return false;
 }
 
-UserManager.prototype.findUserByID = function (userID) {
+UserManager.prototype.findUserByID = function (userID, mycallback) {
     if (userID === undefined || userID < 0 || userID >= this.users.size)
         return null;
-
-            return this.users[userID];
-            // SLOW MODE:
-//            for(var i = 0; i < this.users.length; i++) {
-//                var user = users[i];
-//                if(user.userID === userID) {
-//                    return user;
-//                } else {
-//                    return null;
-//                }
-//            }
-        }
+    client.hgetall("user:" + userID, function(err, user) {
+        console.log(err);
+        mycallback(err, user);
+    });
+}
 
 exports.UserManager = UserManager;
